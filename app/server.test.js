887134e6975bb2b43c3ff5fb61c4b366e1c887eb
Raphael Laucene <40581808+Raphaellaucene@users.noreply.github.com>
2024-11-27 07:47:36 -0300

    Update server.test.js

--- app/server.test.js
+++ app/server.test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const path = require('path');
 const fs = require('fs')
 
@@ -16,22 +15,3 @@ test('.gitignore file exists', () => {
   const filePath = path.join(__dirname, "..", ".gitignore")
   expect(fs.existsSync(filePath)).toBeTruthy();
 });
-=======
-const path = require('path');
-const fs = require('fs')
-
-test('main index.html file exists', () => {
-  const filePath = path.join(__dirname, "index.html")
-  expect(fs.existsSync(filePath)).toBeTruthy();
-});
-
-test('Dockerfile exists', () => {
-  const filePath = path.join(__dirname, "..", "Dockerfile")
-  expect(fs.existsSync(filePath)).toBeTruthy();
-});
-
-test('.gitignore file exists', () => {
-  const filePath = path.join(__dirname, "..", ".gitignore")
-  expect(fs.existsSync(filePath)).toBeTruthy();
-});
->>>>>>> f7429e1 (Initial commit)
